956513c40f12077c21faba5ae8001f543df143a5
Guilherme Goulart <guigagoulart@hotmail.com>
2022-05-28 03:04:53 -0300

    Added Hilt for DI, Retrofit for API Calls and Models

---
+++ app/src/main/java/com/example/postlist/di/RetrofitModule.kt
@@ -0,0 +1,26 @@
+package com.example.postlist.di
+
+import com.example.postlist.services.PostApiInterface
+import com.example.postlist.utils.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+
+    @Singleton
+    @Provides
+    fun provideNewsApiClient(): PostApiInterface {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(PostApiInterface::class.java)
+    }
+}
---
+++ app/src/main/java/com/example/postlist/models/PostList.kt
@@ -0,0 +1,3 @@
+package com.example.postlist.models
+
+class PostList : ArrayList<Post>()
