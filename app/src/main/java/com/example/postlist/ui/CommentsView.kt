605f657c12c96c86bfcbc1bf262b10acc479f8fc
Guilherme Goulart <guigagoulart@hotmail.com>
2022-05-31 00:59:20 -0300

    update on ui

--- app/src/main/java/com/example/postlist/ui/CommentsView.kt
+++ app/src/main/java/com/example/postlist/ui/CommentsView.kt
@@ -8,8 +8,8 @@ import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.Toast
-import androidx.activity.viewModels
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -26,7 +26,7 @@ class CommentsView(
 ) : DialogFragment() {
 
     private lateinit var binding: CommentsViewBinding
-  //  private val viewModel: MainViewModel by viewModels()
+    private val viewModel: MainViewModel by viewModels()
     private lateinit var commentAdapter: CommentAdapter
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
@@ -36,18 +36,15 @@ class CommentsView(
         builder.setView(binding.root)
 
         val dialog = builder.create()
-        dialog!!.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         binding.tvCommentsTitle.text = "O que a galera comentou sobre o post " + title
 
-
-
         initRecycler()
-      //  createComments()
+        createComments()
 
         return dialog
     }
 
-   /* private fun createComments() {
+    private fun createComments() {
         viewModel.getCommentList(id)
         viewModel.commentList.observe(this) { response ->
             if (response.data != null) {
@@ -61,7 +58,7 @@ class CommentsView(
                 ).show()
             }
         }
-    }*/
+    }
 
     private fun initRecycler() {
         binding.rvComments.layoutManager =
